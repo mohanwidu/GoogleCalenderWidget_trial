415b60d3f7c52227d36c965af218b6323e06d891
ananthhh <muruganantham.1989@gmail.com>
2014-08-28 01:52:32 +0530

    'test'

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = { 
 	ddOneOptions : ["DD1-OPTION-1","DD1-OPTION-2"],
 	ddTwoOptions : ["DD2-OPTION-1","DD2-OPTION-2"],
-	extDomains : ["domain1.com","domain2.com"] 
+	extDomains : ["ust-global.com"] 
 };
