02dee6b1ef8e13fb8ec0bc970359b48a8ac50dd6
ananthhh <muruganantham.1989@gmail.com>
2014-09-02 23:48:17 +0530

    'test'

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var http = require('http');
 //2 
 http.createServer(function (req, res) {
   console.log('test');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,      Accept");
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('12345');
 }).listen(3000);
