d6457bbb227c370b5787df37c2dcc28da3588824
ananthhh <muruganantham.1989@gmail.com>
2014-09-02 23:23:36 +0530

    'test'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   console.log('test');
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body><h1>Hello World</h1></body></html>');
+  res.end('12345');
 }).listen(3000);
  
 console.log('Server running on port 3000.');
